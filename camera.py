587157621e1e0780773de65f7ae45da1e66ed0a0
Stefan Frijters <sfrijters@gmail.com>
2019-07-19 23:36:02 +0200

    Update cv2.findCountours call for opencv 3.2

--- camera.py
+++ camera.py
@@ -107,7 +107,7 @@ def main():
         # in holes, then find contours on thresholded image
         thresh = cv2.threshold(frameDelta, config["delta_thresh"], 255, cv2.THRESH_BINARY)[1]
         thresh = cv2.dilate(thresh, None, iterations=2)
-        cnts, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        _, cnts, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         # loop over the contours
         frameIsOccupied = False
